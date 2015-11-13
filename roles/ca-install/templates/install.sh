344648c622f800f8df6090ac2fa938eaf4883b97
Andrew Brunette <andrew.brunette@ctl.io>
2015-11-13 09:19:05 -0800

    adding ansible vars

--- roles/ca-install/templates/install.sh
+++ roles/ca-install/templates/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DATABASE_ADMIN_USERNAME="${1}"
-DATABASE_ADMIN_PASSWORD="${2}"
-UIM_ADMINISTRATOR_USER_NAME="${3}"
-UIM_ADMINISTRATOR_USER_PASSWORD="${4}"
+DATABASE_ADMIN_USERNAME="{{ db_admin_username }}"
+DATABASE_ADMIN_PASSWORD="{{ db_admin_password }}"
+UIM_ADMINISTRATOR_USER_NAME="{{ uim_admin_username }}"
+UIM_ADMINISTRATOR_USER_PASSWORD="{{ uim_admin_password }}"
 yum-complete-transaction -y
 yum-complete-transaction -y
 yum-complete-transaction -y
 
 cd /uim
 
-PW='QWzxPOmn0987!@#$'
+PW='{{ fileshare_password }}'
 mount -t cifs -o domain='t3n',username='svc_ecosys',password=`echo $PW` //172.17.1.23/Software/Ecosystem_Images /mnt
 
 cp /mnt/CA/UIM/uim.zip .
@@ -19,15 +19,15 @@ unset PW
 
 unzip -o uim.zip
 
-perl -p -i -e "s/DB_ADMIN_USER=root/DB_ADMIN_USER=${1}/ig" NMS_installer.properties
-perl -p -i -e "s/DB_ADMIN_PASSWORD=nimsoft/DB_ADMIN_PASSWORD=${2}/ig" NMS_installer.properties
-perl -p -i -e "s/NIM_USER=administrator/NIM_USER=${3}/ig" NMS_installer.properties
-perl -p -i -e "s/NMS_PASSWORD=nimsoft/NMS_PASSWORD=${4}/ig" NMS_installer.properties
-perl -p -i -e "s/NIMBUS_USERNAME=administrator/NIMBUS_USERNAME_USER=${3}/ig" UMP_installer.properties
-perl -p -i -e "s/NIMBUS_PASSWORD=nimsoft/NIMBUS_PASSWORD=${4}/ig" UMP_installer.properties
-perl -p -i -e "s/nimsoft/${2}/ig" mysql-init.sql
+perl -p -i -e "s/DB_ADMIN_USER=root/DB_ADMIN_USER={{ db_admin_username }}/ig" NMS_installer.properties
+perl -p -i -e "s/DB_ADMIN_PASSWORD=nimsoft/DB_ADMIN_PASSWORD={{ db_admin_password }}/ig" NMS_installer.properties
+perl -p -i -e "s/NIM_USER=administrator/NIM_USER={{ uim_admin_username }}/ig" NMS_installer.properties
+perl -p -i -e "s/NMS_PASSWORD=nimsoft/NMS_PASSWORD={{ uim_admin_password }}/ig" NMS_installer.properties
+perl -p -i -e "s/NIMBUS_USERNAME=administrator/NIMBUS_USERNAME_USER={{ uim_admin_username }}/ig" UMP_installer.properties
+perl -p -i -e "s/NIMBUS_PASSWORD=nimsoft/NIMBUS_PASSWORD={{ uim_admin_password }}/ig" UMP_installer.properties
+perl -p -i -e "s/nimsoft/{{ db_admin_password }}/ig" mysql-init.sql
 
-./provision.sh $2
+./provision.sh {{ db_admin_password }}
 
 exit 0
 
